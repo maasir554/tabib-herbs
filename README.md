79b2336b19891df41d4ec86eb83cf389bd9b5b09
MohammadMaasir <maasir554@gmail.com>
2025-01-06 21:34:47 +0530

    initial project setup

---
+++ README.md
@@ -0,0 +1,7 @@
+- don't forget to run
+    ```npm install```
+    when you clone to install node modules.
+
+- always run  
+    ```npm run style```
+    command before commiting the code, to build the css file from tailwindcss.
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors:{
+        'primary': '#297345',
+        'secondary': '#7DC11B'
+      }
+    },
+  },
+  plugins: [],
+}
+
