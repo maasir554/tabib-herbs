8aa43d1412861b66fa41d13080e9f8d09242ba72
MohammadMaasir <maasir554@gmail.com>
2025-01-12 20:38:06 +0530

    make carousel

--- script.js
+++ script.js
@@ -56,3 +56,32 @@ submitButton.addEventListener('click', (event) => {
     window.open(`https://wa.me/919131355378?text=${whatsAppMessage}`, '_blank');
 });
 
+
+const swiper = new Swiper('#carousel-display', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  autoplay: {
+      delay: 2500, // ms delay between transitions
+      disableOnInteraction: false, // Continue autoplay after user interaction
+    },
+
+  // If we need pagination
+  pagination: {
+    el: '#carousel-bullets',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#carousel-right',
+    prevEl: '#carousel-left',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
